5d180ae3e065fa210bbc2900cd6464bc427199d9
Aniket Verma <62088467+aniketverma11@users.noreply.github.com>
2023-04-27 14:07:32 +0530

    Update urls.py

--- remove_bg/urls.py
+++ remove_bg/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("app/", include("app.urls"))
+    path("", include("app.urls"))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
